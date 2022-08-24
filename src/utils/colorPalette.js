35b2447b7de5116a7d87f91b1c7f6484c5429707
devyouth94 <devyouth94@gmail.com>
2022-08-24 20:31:16 +0900

    상수값 모아놓은 파일 추가, 컬러값 추가

--- src/utils/colorPalette.js
+++ src/utils/colorPalette.js
@@ -8,3 +8,4 @@ export const GRAY_8 = "#DDDDDD";
 export const GRAY_7 = "#EBEBEB";
 export const GRAY_6 = "#9D9D9D";
 export const YELLOW = "#FFA726";
+export const PINK = "#F2074C";
---
+++ src/utils/selectList.js
@@ -0,0 +1,5 @@
+const selectCategory = ["모텔", "호텔", "한옥", "펜션", "게스트하우스", "캠핑"];
+const selectLocation = ["서울", "인천", "경기", "강원", "대전", "세종", "충남", "충북", "광주", "전남", "전북", "울산", "부산", "대구", "경남", "경북", "제주"];
+const selectKeyword = ["오션뷰", "파티룸", "야외수영장", "애견동반", "조식", "호캉스"];
+
+export { selectCategory, selectLocation, selectKeyword };
